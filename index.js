de11878e0470669266d5c5ca58c4f90dba77e72c
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-09-03 15:02:10 +0700

    Add better color rng

--- index.js
+++ index.js
@@ -26,19 +26,16 @@ let codeMirrorView;
 let provider;
 let currentState = {}
 
-export const usercolors = [
-    {color: '#30bced', light: '#30bced33'},
-    {color: '#6eeb83', light: '#6eeb8333'},
-    {color: '#ffbc42', light: '#ffbc4233'},
-    {color: '#ecd444', light: '#ecd44433'},
-    {color: '#ee6352', light: '#ee635233'},
-    {color: '#9ac2c9', light: '#9ac2c933'},
-    {color: '#8acb88', light: '#8acb8833'},
-]
+const randomColor = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  const color = "#" + randomColor;
+  const light = color + "33";
+  return {color, light}
+}
 
 const STATUS_COLORS = ["#a83232", "#32a852"]
 
-export const userColor = usercolors[random.uint32() % usercolors.length]
+export const userColor = randomColor()
 
 const getEnterState = () => {
     return {
